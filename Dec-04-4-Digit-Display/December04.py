3f7b69d41abad01fb9844f3ba1d535fce7f4f5ad
Stephen Thirlwall <sdt@dr.com>
2018-12-04 20:17:01 +1100

    Day 4 - 4 Digit 7 Segment LED

--- Dec-04-4-Digit-Display/December04.py
+++ Dec-04-4-Digit-Display/December04.py
@@ -1,4 +1,19 @@
 # December 4 2018
-from microbit import pin1, pin2
+from microbit import pin1, pin2, sleep, button_a, button_b
 from tm1637 import TM1637
-tm = TM1637(clk=pin1, dio=pin2)
+
+stopwatch = TM1637(clk=pin1, dio=pin2)
+running = True
+time = 0
+
+while True:
+    if button_b.was_pressed():
+        time = 0
+    if button_a.was_pressed():
+        if running:
+            running = False
+        else:
+            running = True
+    if running:
+        time = time + 1
+    stopwatch.number(time)
