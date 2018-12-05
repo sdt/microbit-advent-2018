1b70320314fb37a08d0ec0f9e48848036c35b5e7
Stephen Thirlwall <sdt@dr.com>
2018-12-05 17:42:12 +1100

    Day 5 - Potentiometer

--- Dec-05-Potentiometer/December05.py
+++ Dec-05-Potentiometer/December05.py
@@ -1,3 +1,8 @@
 # December 5 2018
 from microbit import *
 
+while True:
+    ppopp = pin0.read_analog()
+    pin1.write_analog(ppopp)
+    print((ppopp, ppopp/8, - ppopp, -ppopp/8))
+    sleep(150)
